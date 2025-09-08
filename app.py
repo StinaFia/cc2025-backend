e7f5ea639863ca7d9e256ebb78ef73b98cf08c4f
StinaFia <stina.carlqvist@edu.lapinamk.fi>
2025-09-08 15:46:56 +0100

    port change

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def get_data():
     return {"message": "Data received", "data": data}
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="8080", debug=True)
