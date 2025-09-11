13298143498e9ef1916138fef9224b63f747cd42
StinaFia <stina.carlqvist@edu.lapinamk.fi>
2025-09-11 06:14:00 +0100

    version 1.2

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     print('hello')
-    return "<p>Hello, World! Version 1.1</p>"
+    return "<p>Hello, World! Version 1.2</p>"
 
 # demonstration how to get JSON data from the user
-@app.route('/data', methods=['POST'])
-def get_data():
-    data = request.json
+@app.route('/sentiment', methods=['POST'])
+def get_sentiment():
+    input_data = request.json
     # access the posted data
-    print(data["name"])
-    return {"message": "Data received", "data": data}
+    print(input_data)
+    return {"input_data": input_data, "message": "Hello!"}
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8080", debug=False)
